22f7febe2881bf5e8a36e104103dd4bd638da6eb
markng <stngad@connect.ust.hk>
2023-05-15 19:44:29 +0800

    add some intro

--- traceroute.py
+++ traceroute.py
@@ -1,6 +1,15 @@
 import sys
+import socket
+
+def traceroute(domain, max_ttl):
+    ip = socket.gethostbyname(domain)
+    print(f"traceroute to {domain} ({ip}), {max_ttl} hops max")
 
 if __name__ == '__main__':
     assert len(sys.argv) == 2
-    target = sys.argv[1]
-    print(target)
+    domain = sys.argv[1]
+
+    # Config
+    max_ttl = 64
+
+    traceroute(domain, max_ttl)
